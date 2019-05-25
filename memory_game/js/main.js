1e834a93fb7e3f8fbf0fa02e0369f24d131e73d5
Ahmed <sport996@hotmail.com>
2019-05-25 08:38:04 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -18,21 +18,33 @@ var cards = [{
 }
 	];
 var cardsInPlay = [];
+
 function checkForMatch(){
+	this.setAttribute('src',cards[cardId].cardImage);
 	if (cardsInPlay[0] === cardsInPlay[1])
 		console.log("You found a match!");
 	else
   		console.log("Sorry, try again.");
 };
-function flipCard(cardId){
+
+function flipCard(){
+	cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);	
 	cardsInPlay.push(cards[cardId].rank);
-	if(cardsInPlay.length === 2){		
+	if (cardsInPlay.length === 2){		
 		checkForMatch();
 	}		
 };
 
-flipCard(0);
-flipCard(2);
+function createBoard(){
+	for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src','images/back.png');
+    cardElement.setAttribute('data-id',i);
+    cardElement.addEventListener('click',flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+}
+};
+createBoard();
